cd99f136049848da84701043d0ca8500b72e4961
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-01-29 14:56:08 +0530

    Implement inventory management functionality: add InventoryController, InventoryServiceImpl, InventoryMapper, and InventoryRepository; define CRUD operations for inventory items

---
+++ src/main/java/edu/icet/service/InventryService.java
@@ -0,0 +1,15 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.InventoryDto;
+import edu.icet.model.dto.InventoryRequestDto;
+
+import java.util.List;
+
+public interface InventryService {
+
+    void addInventry(InventoryRequestDto requestDto);
+    void updateInventory(Long id, InventoryRequestDto requestDto);
+    void deleteInventory(Long id);
+    List<InventoryDto> getAllInventory();
+
+}
