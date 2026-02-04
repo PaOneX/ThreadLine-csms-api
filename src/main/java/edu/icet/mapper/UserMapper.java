e52e40f78492e7e64c58a922d0e1201831b66f25
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-02-04 18:20:26 +0530

    Refactor RoleMapper and UserMapper: enhance role mapping methods and ignore specific fields during user entity conversion

--- src/main/java/edu/icet/mapper/UserMapper.java
+++ src/main/java/edu/icet/mapper/UserMapper.java
@@ -4,17 +4,23 @@ import edu.icet.model.dto.user.UserDto;
 import edu.icet.model.dto.user.UserRequestDto;
 import edu.icet.model.entity.User;
 import org.mapstruct.Mapper;
-import org.springframework.data.domain.Page;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {RoleMapper.class})
 public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "roles", source = "roles")
     User toEntity(UserRequestDto userRequestDto);
 
-    List<UserDto> toDtoList(List<User> userEntity);
-
+    @Mapping(target = "roles", source = "roles")
     UserDto toDto(User user);
 
-    Page<UserDto> toDtoPage(Page<User> userPage);
+    List<UserDto> toDtoList(List<User> users);
 }
