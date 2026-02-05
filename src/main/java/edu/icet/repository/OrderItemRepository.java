f7b6369e80bcc3ad844ec6e5f9a959cad2391a56
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-05 14:53:22 +0530

    Enhance OrderItemRepository: add methods for saving, finding by ID, and deleting OrderItem entities for improved data management

--- src/main/java/edu/icet/repository/OrderItemRepository.java
+++ src/main/java/edu/icet/repository/OrderItemRepository.java
@@ -4,7 +4,9 @@ import edu.icet.model.entity.Order;
 import edu.icet.model.entity.OrderItem;
 import edu.icet.model.entity.ProductVariant;
 
+import java.lang.ScopedValue;
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderItemRepository {
     List<OrderItem> findAllByOrder(Order order);
@@ -12,4 +14,10 @@ public interface OrderItemRepository {
     List<OrderItem> findAllByOrderId(Long orderId);
 
     List<OrderItem> findAllByVariant(ProductVariant variant);
+
+    void save(OrderItem entity);
+
+    Optional<OrderItem> findById(Long itemId);
+
+    void delete(OrderItem orderItem);
 }
