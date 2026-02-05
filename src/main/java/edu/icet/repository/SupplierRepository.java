7fa98b59f0fbcec4190a379f8c8bf93393fd31ab
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-05 15:00:22 +0530

    Enhance SupplierRepository: add methods for saving, finding by ID, checking existence, deleting by ID, and finding all suppliers for improved data management

--- src/main/java/edu/icet/repository/SupplierRepository.java
+++ src/main/java/edu/icet/repository/SupplierRepository.java
@@ -2,6 +2,18 @@ package edu.icet.repository;
 
 import edu.icet.model.entity.Supplier;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SupplierRepository {
 
+    void save(Supplier entity);
+
+    Optional<Supplier> findById(Long id);
+
+    boolean existsById(Long id);
+
+    void deleteById(Long id);
+
+    List<Supplier> findAll();
 }
