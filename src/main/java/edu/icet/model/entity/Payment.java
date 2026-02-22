3029d0dfa921119d6afb296dd032091f7ed782ed
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-22 21:49:13 +0530

    Refactor Orders and Payment repositories: implement CRUD operations, enhance query methods, and update method visibility for improved clarity and functionality; comment out validation annotations and Swagger documentation for potential future use

--- src/main/java/edu/icet/model/entity/Payment.java
+++ src/main/java/edu/icet/model/entity/Payment.java
@@ -27,7 +27,7 @@ public class Payment {
 
     private LocalDateTime createdAt;
 
-    protected void onCreate() {
+    public void onCreate() {
         createdAt = LocalDateTime.now();
         if (paymentDate == null) {
             paymentDate = LocalDateTime.now();
